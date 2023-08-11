0f7188b974955a81844a876f7949fb9405f4d4a8
Kei Fujikawa <fujikei22funk@gmail.com>
2023-08-11 12:07:20 +0900

    Initial Commit

---
+++ ScreenTimeAPIDemo3/ScreenTimeAPIDemo3App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeAPIDemo3App.swift
+//  ScreenTimeAPIDemo3
+//
+//  Created by Kei Fujikawa on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeAPIDemo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
